6bc57d467026c99a69a7068a78cd97c5a1b7098a
NodeProject <mubeenhazara7844@gmail.com>
2024-10-28 12:37:22 +0500

    text weight and color

--- src/app/components/HotelBanner.jsx
+++ src/app/components/HotelBanner.jsx
@@ -38,7 +38,7 @@ const flighttabs = [
     id: 5,
     name: "Packages",
     pic: <TbPackages color="white" />,
-  },
+  }
 ];
 
 const HotelBanner = () => {
@@ -61,19 +61,19 @@ const HotelBanner = () => {
         ))}
 
       </div>
-      <h1 className="my-[40px] text-4xl font-semibold">Find the right <span className="text-green-600">hotel today</span></h1>
+      <h1 className="my-[40px] text-4xl font-semibold text-white">Find the right <span className="text-green-600">hotel today</span></h1>
       <div className="w-full bg-green-600 p-10 xs:p-7 rounded">
         <form>
           <div className="grid grid-cols-12">
             <div className="xl:col-span-10 lg:col-span-12 xs:col-span-12 col-span-12 grid grid-cols-12">
 
               <div className="destinations col-span-12 xl:col-span-5">
-                <label className="flex justify-start mb-2">Where do you want to stay?</label>
+                <label className="text-white flex justify-start mb-2 font-semibold">Where do you want to stay?</label>
                 <input className="xl:border-r w-full xs:rounded p-3 text-gray-71 lg:rounded-tl lg:rounded-bl lg:rounded-tr-none lg:rounded-br-none outline-none" type="text" placeholder="Enter destination or hotel name" />
               </div>
 
               <div className="Check_in col-span-12 xs:mt-0 xl:col-span-2">
-                <label className="flex justify-start mb-2">Check in</label>
+                <label className="text-white flex justify-start mb-2 font-semibold">Check in</label>
                 <DatePicker
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
@@ -82,7 +82,7 @@ const HotelBanner = () => {
               </div>
 
               <div className="Check_out col-span-12 xs:mt-0 xl:col-span-2">
-                <label className="flex justify-start mb-2">Check in</label>
+                <label className="text-white flex justify-start mb-2 font-semibold">Check in</label>
                 <DatePicker
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
@@ -91,7 +91,7 @@ const HotelBanner = () => {
               </div>
 
               <div className="sel_booking col-span-12 xs:mt-0 xl:col-span-3">
-                <label className="flex justify-start mb-2">Guests and rooms</label>
+                <label className="text-white flex justify-start mb-2 font-semibold">Guests and rooms</label>
                 <select id="countries" class="xs:rounded lg:rounded-tl-none lg:rounded-bl-none lg:rounded-tr lg:rounded-br rounded text-gray-71 bg-white text-gray-900 text-sm py-[14.5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                   <option selected>Choose a country</option>
                   <option value="US">United States</option>
