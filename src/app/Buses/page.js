15cd89b82a3896fdc2457c79b315c39add9cee9e
mathertahir <mianmather249@gmail.com>
2024-11-04 18:42:21 +0500

    Buses Banner In Progress

---
+++ src/app/Buses/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BusesBaner from "../components/BusesBaner";
+
+const page = () => {
+  return (
+    <div>
+      <BusesBaner />
+    </div>
+  );
+};
+
+export default page;
